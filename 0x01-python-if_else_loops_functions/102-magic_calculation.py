812c3e7208cd24766544f721393d3f002b5a9838
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-10 19:47:31 -0400

    Magic Add

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(num1, num2, num3):
+    if num1 < num2:
+        return(num3)
+    elif num3 > num2:
+        return(num1 + num2)
+    else:
+        return(num1 * num2 - num3)

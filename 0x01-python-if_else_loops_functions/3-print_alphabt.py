0c4a09c2dd38560e6a161a99fea70ca19bd93b5f
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-10 17:23:40 -0400

    Print Alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for Mynum in range(ord('a'), ord('z') + 1):
     if chr(Mynum) != 'e' and chr(Mynum) != 'q':
-        print("{:c}".format(a), end='')
+        print("{:c}".format(Mynum), end='')

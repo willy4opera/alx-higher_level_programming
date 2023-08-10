806afcc12ea82c2bcea847c2902060f7266ed772
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-10 19:32:12 -0400

    Print Teb

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for charc in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((charc - (ord('a') - ord('A'))) if charc % 2 else charc), end='')
+for ch in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((ch - (ord('a') - ord('A'))) if ch % 2 else ch), end='')

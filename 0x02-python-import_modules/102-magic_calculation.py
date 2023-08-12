0434452caf698885daa36443cad15249fc1e54c4
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-11 22:33:51 -0400

    Debbuged the 102-magic_calculation.py function

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+        from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
+        for num in range(4, 6):
+            c = add(c, num)
         return (c)
 
     else:

da81d442331bc2f4b6dc77152865a3870a541062
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-11 21:57:56 -0400

    Added the 3-infinite_add.py function

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    sum = 0
+    for num in range(len(sys.argv) - 1):
+        sum += int(sys.argv[num + 1])
+    print("{}".format(sum))

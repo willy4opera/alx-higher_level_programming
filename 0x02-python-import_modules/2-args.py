19700c5d65df1d7a0f8d1a2fa54116770c6a4c47
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-11 21:54:07 -0400

    Added the 2-args.py function

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    cnt = len(sys.argv) - 1
+    if cnt == 0:
+        print("0 arguments.")
+    elif cnt == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(cnt))
+    for num in range(cnt):
+        print("{}: {}".format(num + 1, sys.argv[num + 1]))

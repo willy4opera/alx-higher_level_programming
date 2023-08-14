dca97992d58cb5f7bed3acd9cd1da82d81063fac
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-14 18:27:15 -0400

    Debbuged Element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+# Function retrieves an element from a List.
+
 def element_at(my_list, idx):
-    if (idx < 0) or (idx > len(my_list) - 1):
-        return None
-    return(my_list[idx])
+    if idx < 0 or idx >= len(my_list):
+        return
+    return my_list[idx]

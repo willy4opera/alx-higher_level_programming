dbc28bfe078981cf01943497bfc835bb80c1201c
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-14 19:31:07 -0400

    Debuuuged Funtion main

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#Matrix Intege
+
+def print_matrix_integer(matrix=[[]]):
+    for num in range(len(matrix)):
+        for num2 in range(len(matrix[num])):
+            Mprint("{:d}".format(matrix[num][nu2j]), end="")
+            if num2 != (len(matrix[num]) - 1):
+                print(" ", end="")
+
+        print("")

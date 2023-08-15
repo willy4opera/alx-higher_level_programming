f212ace282210d2c1a763c26c3057c638cca5b45
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-14 23:00:22 -0400

    Deburged Returnsn

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-
 def multiple_returns(sentence):
-    """Returns the length of a string and its first character."""
-   if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    dev_tuple = ()
+    if len(sentence) == 0:
+        dev_tuple = 0, "None"
+    else:
+        dev_tuple = len(sentence), sentence[0]
+    return dev_tuple

9d8f478f4359b28fcae293f162040870896ace86
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-16 17:54:16 -0400

    3-common_elements.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-# adds all unique integers in a list 
+# Adds all unique integers in a list 
 
 def uniq_add(my_list=[]):
     result = 0
-    for num in set(my_list):
-        result += num
+    for a in set(my_list):
+        result += a
     return result

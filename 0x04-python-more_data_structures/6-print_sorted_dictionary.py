6a572a8a238a7daa8bc0c1ba090735f37d7540ae
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-16 19:23:47 -0400

    Debbuged 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+#  A function that prints a dictionary by ordered keys.
 
 def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(num, a_dictionary[num])) for num in sorted(a_dictionary)]
+    [print("{}: {}".format(n, a_dictionary[n])) for n in sorted(a_dictionary)]

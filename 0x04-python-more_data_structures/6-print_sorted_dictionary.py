e5cf7b42b0a0f7ea83c8dcc09a31fc56ac5d91ec
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-16 19:20:32 -0400

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+    [print("{}: {}".format(num, a_dictionary[num])) for num in sorted(a_dictionary)]

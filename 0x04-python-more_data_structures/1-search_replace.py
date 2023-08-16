1bab4992d1d408963b0bf36c85ee5604424bda24
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-16 17:09:40 -0400

    a function that adds all unique integers in a list (only once for each integer).

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Search and Replace
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for num in range(len(new_list)):
+        if new_list[num] == search:
+            new_list[num] = replace
+    return new_list

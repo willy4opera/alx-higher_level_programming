36896383cf42dfb7f744ce8208931053f5e3058e
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-29 23:50:50 -0400

    Debugged 103-python.c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,8 @@
 #include <Python.h>
 #include <stdio.h>
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
 
 /**
  * print_python_bytes - gives data of the PyBytesObject

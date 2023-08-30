515df0d1bfebcd4cd95160665638f7b8ca4c1fa3
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-08-30 00:48:05 -0400

    Added readme

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""The Square Class."""
+
+
+class Square:
+    """Represent a square Here."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""The Square Class."""
+
+
+class Square:
+    """Represent a square Here."""
+
+    def __init__(self, size):
+        """Initialize a new Square here.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size

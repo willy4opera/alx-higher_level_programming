56a39d1409e81d59bc9a5bbdc8f6b7f4c97d22f4
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-09-05 18:38:20 -0400

    Project completed 6

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def magic_string():
-    mgc_strng.n = getattr(mgc_strng, 'n', 0) + 1
-    return ("BestSchool, " * (mgc_strng.n - 1) + "BestSchool")
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")

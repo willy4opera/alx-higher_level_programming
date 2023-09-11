fb9a3bb37fe44ee6a520c422888112ff220a8a79
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-09-11 10:50:49 -0400

    1-my_list.py

---
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Here we defined the inherited list class MyList."""
+
+
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
+
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))

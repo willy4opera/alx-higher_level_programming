0913b0620eaf0f333dde7481a8f9fa1197bbd39f
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-09-11 14:06:40 -0400

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Here, we defined a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """We Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")

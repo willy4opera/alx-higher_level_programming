4957522fc29542eb2fc18608383906fcf3d8b531
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-09-11 16:55:38 -0400

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Here, we defined a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        res = self.real != value
+        return (res)
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        res = self.real == value
+        return (res)

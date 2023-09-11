fa91cf7391ed446f12e21c8818ad52254f73bb37
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-09-11 10:58:59 -0400

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,5 @@ class MyList(list):
 
     def print_sorted(self):
         """Print a list in sorted ascending order."""
-        print(sorted(self))
+        result = sorted(self)
+        print(result)

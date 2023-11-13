5447a2a40c6145aef3cdcad7a30101e167396465
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-13 13:32:33 -0500

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,15 +1,3 @@
 #!/usr/bin/node
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
-       let myVar = "JavaScript is amazing";
-       console.log(myVar);
-    </script>
-</body>
-</html>       
+let myVar = 'JavaScript is amazing';
+console.log(myVar); 

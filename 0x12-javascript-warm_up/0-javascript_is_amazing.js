c16ea54e8399c76ab63535e986281fe137de1b78
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-13 13:22:19 -0500

    0-javascript_is_amazing.js

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+       let myVar = "JavaScript is amazing";
+       console.log(myVar);
+    </script>
+</body>
+</html>       
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,4 @@
+# PROJECT AUTHOR: ODIONYE OBIAJULU WILLIAMS
+# PROJECT TITLE: JAVASCRIP BASICS
+
+This Directory contains functions written for javascript

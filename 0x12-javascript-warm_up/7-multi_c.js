db630308ba415a33b989f3066ebed94cf558ebbf
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-13 18:41:27 -0500

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const prc = process.argv[2];
+const prcisn = isNaN(process.argv[2]
+if (prc === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = Number(prc);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}

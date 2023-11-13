3ded38d68b06e0b4b8a4b4fbb8a1cfdd446c0cb3
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-13 18:56:02 -0500

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const prc2 = process.argv[2];
+const prc3 = isNaN(process.argv[2]);
+if (prc2 === undefined || prcs3 ) {
+  console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
+}

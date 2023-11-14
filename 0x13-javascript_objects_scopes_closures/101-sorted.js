732f13dad5ea1f0a7b600c7c2cbbb675edf3bd4c
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-14 17:09:21 -0500

    Added the 101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const totalist = Object.entries(dict);
+const val = Object.values(dict);
+const Uniqval = [...new Set(val)];
+const NewDict = {};
+for (const numj in Uniqval) {
+  const list = [];
+  for (const numk in totalist) {
+    if (totalist[numk][1] === Uniqval[numj]) {
+      list.unshift(totalist[numk][0]);
+    }
+  }
+  NewDict[Uniqval[numj]] = list;
+}
+console.log(NewDict);

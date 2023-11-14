42945d05029bcd29c8f8c32f645f5bc7d4fbad19
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-13 23:09:57 -0500

    7-multi_c.js fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const prc = process.argv[2];
-const prcisn = isNaN(process.argv[2]
-if (prc === undefined || isNaN(process.argv[2])) {
+const prcisn = isNaN(process.argv[2]);
+if (prc === undefined || prcisn) {
   console.log('Missing number of occurrences');
 } else {
   const x = Number(prc);

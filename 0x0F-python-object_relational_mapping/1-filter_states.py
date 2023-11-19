0469339bc973209049be0eb05410d23c2042a421
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-19 18:19:28 -0500

    debbuged the select-0 task debugged

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+
+"""  Here, we,lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     dev_conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3],
                                port=3306)
-    curr = dev_conn.cursor()
+    dev_curr = dev_conn.cursor()
     dev_cur.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
     rows = dev_cur.fetchall()

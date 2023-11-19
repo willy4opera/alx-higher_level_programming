3d35ab7e9753094fff7f55cc3d0af927189a9b90
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-19 17:48:10 -0500

    debbuged the select-0 task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     dev_conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
     dev_cur = dev_conn.cursor()
     dev_cur.execute("SELECT * FROM states")
     rows = dev_cur.fetchall()

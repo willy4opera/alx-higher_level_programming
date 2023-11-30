c0613406259bc03d1ff0572f357ba85404843ec0
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-11-30 16:42:21 -0500

    added 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This function Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Finds the peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    d_low = 0
+    d_high = len(list_of_integers)
+    d_mid = ((d_high - d_low) // 2) + d_low
+    d_mid = int(d_mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if d_high == 2:
+        return max(list_of_integers)
+    if list_of_integers[d_mid] >= list_of_integers[d_mid - 1] and\
+            list_of_integers[d_mid] >= list_of_integers[d_mid + 1]:
+        return list_of_integers[d_mid]
+    if d_mid > 0 and list_of_integers[d_mid] < list_of_integers[d_mid + 1]:
+        return find_peak(list_of_integers[d_mid:])
+    if d_mid > 0 and list_of_integers[d_mid] < list_of_integers[d_mid - 1]:
+        return find_peak(list_of_integers[:d_mid])

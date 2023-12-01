6c72d75787253c32e9b901a1c86e80b93d4d5ca0
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-12-01 16:34:18 -0500

    Added the 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,7 +3,8 @@
 """The script:
 takes in a URL,
 sends a request to the URL and displays the value
-of the X-Request-Id variable found in the header ofthe response.
+of the X-Request-Id variable
+found in the header ofthe response.
 """
 import sys
 import urllib.request

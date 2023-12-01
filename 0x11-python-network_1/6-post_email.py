e22a6617e6f315becd64077219f0b68718881f1b
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-12-01 16:45:02 -0500

    Added the 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,12 +6,11 @@ sends a request to the URL and displays the value
 of the X-Request-Id variable
 found in the header ofthe response.
 """
-import sys
-import urllib.request
 
-if __name__ == "__main__":
-    url_re = sys.argv[1]
+from sys import argv
+import requests
 
-    req = urllib.request.Request(url_re)
-    with urllib.request.urlopen(req) as resp:
-        print(dict(resp.headers).get("X-Request-Id"))
+
+if __name__ == '__main__':
+    response = requests.post(argv[1], {'email': argv[2]})
+    print(response.text)

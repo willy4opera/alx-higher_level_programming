c4a5873c1a7aed4a1e14a4610f74c0aa7dc503bf
willyopera <willy4opera@gmail.com>
2024-01-03 14:54:35 -0500

    Updated the 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-//Create A new Files System Object filesys
-
 const filesys = require('fs');
 filesys.writeFile(process.argv[2], process.argv[3], error => {
   if (error) console.log(error);

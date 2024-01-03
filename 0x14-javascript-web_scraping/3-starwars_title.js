378966694f0547ed2855cc18c9e6a9bb3d1bec57
willyopera <willy4opera@gmail.com>
2024-01-03 17:43:58 -0500

    Added the 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const request = require('request');
-const episodeNum = process.argv[2];
+const req = require('request');
+const EpisodVal = process.argv[2];
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episodeNum, function (err, response, body) {
+req(API_URL + EpisodVal, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {

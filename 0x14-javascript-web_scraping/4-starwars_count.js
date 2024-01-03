99bfa39b20ac2621a2ccaf33b5ff90d66fb8cfba
willyopera <willy4opera@gmail.com>
2024-01-03 18:06:17 -0500

    Added the 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const req = require('request');
-req(process.argv[2], function (error, response, body) {
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const dresult = JSON.parse(body).dresult;
-    console.log(dresult.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
